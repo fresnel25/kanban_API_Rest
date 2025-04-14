7599b9a49807b75a7801f6ae033a1288db429074
fresnel25 <ngaleufresnel@gmail.com>
2025-04-14 11:33:35 +0200

    init kanban project

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express")
+require('dotenv').config()
+const app = express()
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT,()=>{
+    console.log(`Le serveur tourne sur le port ${PORT}`)
+})
